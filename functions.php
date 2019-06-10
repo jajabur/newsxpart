e66d2255e8c130cbd1f10cc4402ad54672032321
kamrul islam <to.kamrulislam@gmail.com>
2019-06-10 18:52:35 +0600

    create function and widgets php file in inc folder

--- functions.php
+++ functions.php
@@ -98,39 +98,6 @@ function newsxpart_content_width() {
 }
 add_action( 'after_setup_theme', 'newsxpart_content_width', 0 );
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function newsxpart_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'newsxpart' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'newsxpart' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'newsxpart_widgets_init' );
-
-/**
- * Enqueue scripts and styles.
- */
-function newsxpart_scripts() {
-	wp_enqueue_style( 'newsxpart-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'newsxpart-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'newsxpart-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'newsxpart_scripts' );
 
 /**
  * Implement the Custom Header feature.
